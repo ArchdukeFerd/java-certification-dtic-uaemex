8b19607046e84280addb1946b2aad7febc188594
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-10 10:38:04 -0600

    Domain 2.1.1 example

---
+++ ex211.java
@@ -0,0 +1,17 @@
+public class ex211 {
+    /*
+    This is a code example of variables
+    */
+    public static void main(String[] args) {
+        byte bagSize = 12;
+        char roast =  'M'; // This is a single character
+        int beans =  360;
+        double price = 8.99;
+        short qtysold = 3000;
+        long annualQtySold = 36000l;
+        float costPerOz = .49f;
+        boolean caffeine = true;
+
+        System.out.println(bagSize * costPerOz);
+    }
+}
