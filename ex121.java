cf4ca95ffa7fa0cce0ae30147e5838a020ff7fd0
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-09 19:10:42 -0600

    Domain 1.2.2 example

---
+++ ex121.java
@@ -0,0 +1,7 @@
+public class ex121 {
+    public static void main(String[] args) {
+        System.out.println("Coffee");
+        System.out.println("Small\t 1.00");
+        System.out.println("Large\n 2.00");
+    }
+}
