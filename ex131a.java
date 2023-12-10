3a1f69124f2dfb971455c3d2871dab7b424e96c4
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-09 19:21:49 -0600

    Domain 1.3.1 example

---
+++ ex131a.java
@@ -0,0 +1,9 @@
+public class ex131a {
+    public static void main(String[] args) {
+        String location  = "Utah";
+        if (location == "Utah"){
+            double salesTax = .047;
+            System.out.println("Your sales tax rate is " + salesTax * 100 + "%.");
+        }
+    }
+}
