49824538ebbc4ed524f4510bcce10582b69c2a45
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 12:57:38 -0600

    Domain 2.5.1.a example

---
+++ ex251a.java
@@ -0,0 +1,12 @@
+public class ex251a {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 7;
+
+        System.out.println((x + y));
+        System.out.println((x - y));
+        System.out.println((x * y));
+        System.out.println((x / y));
+        System.out.println((x % y));
+    }
+}
