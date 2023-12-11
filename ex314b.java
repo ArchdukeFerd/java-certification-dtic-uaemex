698e3b04a4b2617d77b336fd7e13b5f280915d17
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 14:43:15 -0600

    Domain 3.1.4.b example

---
+++ ex314b.java
@@ -0,0 +1,13 @@
+public class ex314b {
+    public static void main(String[] args) {
+        double italRoastPrice = 9.99;
+        double frenchRoastPrice = 8.99;
+
+        System.out.println(italRoastPrice == frenchRoastPrice);
+        System.out.println(italRoastPrice != frenchRoastPrice);
+        System.out.println(italRoastPrice >= frenchRoastPrice);
+        System.out.println(italRoastPrice <= frenchRoastPrice);
+        System.out.println(italRoastPrice > frenchRoastPrice);
+        System.out.println(italRoastPrice < frenchRoastPrice);
+    }
+}
