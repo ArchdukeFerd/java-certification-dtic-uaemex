45a70759c4253fd678e949952b85dd3f6bda49a4
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-10 22:03:01 -0600

    Domain 2.2.2 example

---
+++ ex222.java
@@ -0,0 +1,9 @@
+public class ex222 {
+    public static void main(String[] args) {
+        String product1 = "House Blend";
+        String product2 = "Sunatra";
+
+        System.out.println("|" + product1 + "|");
+        System.out.println(String.format("|%20s|", product2));
+    }
+}
