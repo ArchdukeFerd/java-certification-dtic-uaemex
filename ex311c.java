e7b3e7138fd6250fc9dc261ee6c43340b6f36e61
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 14:21:19 -0600

    Domain 3.1.1.c example

---
+++ ex311c.java
@@ -0,0 +1,15 @@
+public class ex311c {
+    public static void main(String[] args) {
+        int amount = 1;
+        
+        if (amount > 2){
+            System.out.println("Enjoy a free coaster with your order.");
+        }
+        else if (amount == 2){
+            System.out.println("Take $2 off your next order.");
+        }
+        else {
+            System.out.println("Thank you for your order");
+        }
+    }
+}
