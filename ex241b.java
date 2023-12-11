9f3f24e0fedd5aa75ae860d257729ea53b05cb37
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 12:55:54 -0600

    Domain 2.4.1.b example

---
+++ ex241b.java
@@ -0,0 +1,10 @@
+public class ex241b {
+    public static void main(String[] args) {
+        String proposedBagSizeString = new String("11.8");
+        double proposedBagSize = Double.parseDouble(proposedBagSizeString);
+        // int proposedBagSizewhole = Integer.parseInt(proposedBagSizeString);
+
+        System.out.println(proposedBagSize);
+        // System.out.println(proposedBagSizewhole);
+    }
+}
