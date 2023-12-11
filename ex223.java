ac72d78d3dc5c921a6fc3adbd318b6d0e408d04b
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-10 22:08:29 -0600

    Domain 2.2.3 example

---
+++ ex223.java
@@ -0,0 +1,10 @@
+public class ex223 {
+    public static void main(String[] args) {
+        String tea1 = "0001       Green Tea       7.59       ";
+        String tea2 = "0002       White Tea       8.99       ";
+
+        System.out.println(tea1.trim());
+        System.out.println(tea2.substring(11,20));
+        System.out.println(tea1.charAt(3));
+    }
+}
