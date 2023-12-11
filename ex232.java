637314dff5cdeb5f1d6f22fabb7fc51ec9f2bc06
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-10 22:36:30 -0600

    Domain 2.3.2 example

---
+++ ex232.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ex232 {
+    public static void main(String[] args) {
+        ArrayList<String> cups = new ArrayList<>();
+
+        cups.add("Silver");
+        cups.add("Black");
+        cups.add("#1");
+        cups.add("Maroon");
+
+        System.out.println(cups.get(1));
+
+        Iterator cupList = cups.iterator();
+        while (cupList.hasNext()){
+            System.out.println(cupList.next());
+        }
+    }
+}
