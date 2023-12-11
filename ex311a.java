659170ee11b337427a82d0fb2b962fa781d95f41
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 14:18:55 -0600

    Domain 3.1.1.b example

--- ex311a.java
+++ ex311a.java
@@ -1,6 +1,6 @@
 public class ex311a {
     public static void main(String[] args) {
-        int amount = 5;
+        int amount = 1;
         
         if (amount >= 2){
             System.out.println("Enjoy a free coaster with your order.");
---
+++ ex311b.java
@@ -0,0 +1,13 @@
+public class ex311b {
+    public static void main(String[] args) {
+        int amount = 2;
+        
+        if (amount > 2){
+            System.out.println("Enjoy a free coaster with your order.");
+        }
+        else {
+            System.out.println("Thank you for your order");
+        }
+    }
+}
+
