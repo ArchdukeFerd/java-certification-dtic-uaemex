ce3e1f9862ccd30fa3e38dcd88f9617389a43bab
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-10 22:31:50 -0600

    Domain 2.3.1.c example

---
+++ ex231c.java
@@ -0,0 +1,11 @@
+public class ex231c {
+    public static void main(String[] args) {
+        String [] cocoaBrands = {"Regular", "Dark Chocolate", null, "Sugar free"};
+
+        System.out.println(cocoaBrands[1]);
+
+        for (String cocoa: cocoaBrands){
+            System.out.println(cocoa);
+        }
+    }
+}
