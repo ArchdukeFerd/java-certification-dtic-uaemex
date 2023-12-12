bd2701bea598ffc27eed97cbcfbbe240d0340e00
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 19:36:15 -0600

    Domain 3.2.4.b example

---
+++ ex324b.java
@@ -0,0 +1,14 @@
+public class ex324b {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        System.out.println(x++); // print x and then increase by one
+        System.out.println(x); // print x that has already been added one
+        System.out.println(++x); // increase x by one and the print it
+
+        // System.out.println(y--); // print y and decrease by one
+        // System.out.println(y); // print y that has already decresased by one
+        // System.out.println(--y); // decrease y by one and then print it
+    }
+}
