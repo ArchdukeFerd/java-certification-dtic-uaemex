16825f842ec581d0eb636f1a6c607aa4bd75840e
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-11 19:29:41 -0600

    Domain 3.2.4.a example

---
+++ ex324a.java
@@ -0,0 +1,22 @@
+public class ex324a {
+    public static void main(String[] args) {
+        String [] territories = new String[2];
+        territories[0] = "U.S.";
+        territories[1] = "International";
+
+        String [] cocoaBrands = new String[4];
+        cocoaBrands[0] = "Regular";
+        cocoaBrands[1] = "Dark Chocolate";
+        cocoaBrands[2] = "Mint Chocolate";
+        cocoaBrands[3] = "Sugar free";
+
+        for (int t = 0; t < territories.length; t++){
+            for (int c = 0; c < cocoaBrands.length; c++){
+                if (territories[t] == "International" && cocoaBrands[c] == "Sugar free"){
+                    continue;
+                }
+                System.out.println(t + "." + c + ". " + territories[t] + " - " + cocoaBrands[c]);
+            }
+        }
+    }
+}
