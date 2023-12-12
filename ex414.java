5a597077265328b4a3a032235e7cbee21335e0ca
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-12 13:30:22 -0600

    Domain 4.1.4 example

---
+++ ex414.java
@@ -0,0 +1,31 @@
+public class ex414 {
+    public static void main(String[] args) {
+    Product prod1 = new Product(1, "Tea", "Green Tea", 8.99);
+    System.out.println("Our first tea is " + prod1.productName + ".");
+
+    // Product prod2 = new Product(2, "Coffee", "House Blend");
+    // System.out.println("The " + prod2.productName + " brand sells for " + prod2.price + ".");
+
+    Product prod3 = new Product();
+    System.out.println("The " + prod3.productName + " brand sells for " + prod3.price + ".");
+    }
+    
+    static class Product {
+    int productId;
+    String type;
+    String productName;
+    double price;
+
+   Product(int productIdValue, String typeValue, String productNameValue, double priceValue){
+        productId = productIdValue;
+        type = typeValue;
+        productName = productNameValue;
+        price = priceValue;
+   }
+
+   Product(){
+    this(3, "coffee", "Decaf", 9.99);
+   }
+    }
+
+}
