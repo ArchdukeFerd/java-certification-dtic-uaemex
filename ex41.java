b49f769fc63de9eb7eac933d2371116adb2e1fc5
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-12 11:30:10 -0600

    Domain 4.1 example

---
+++ ex41.java
@@ -0,0 +1,12 @@
+public class ex41 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Product {
+    int productId;
+    String type;
+    String productName;
+    double price;
+}
