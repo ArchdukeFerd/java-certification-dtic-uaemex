01215e4e50c6c6e3707673a153e1d0ccd41ce3e8
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-13 15:29:07 -0600

    Domain 4.3.8 example

---
+++ ex438.java
@@ -0,0 +1,36 @@
+public class ex438 {
+    static String location = "UT";
+
+    public static void main (String args[]) {
+        double order1 = orderTotal(5, 9.99);
+        double order2 = orderTotal(4, 8.99, "AZ");
+
+        System.out.println("Your first order total is " + String.format("%.2f", order1));
+        System.out.println("Your second order total is " + String.format("%.2f", order2));
+    }
+
+    public static double orderTotal(int qty, double price) {
+        double amount;
+
+        if(location == "UT") {
+            amount = qty * price * 1.06;
+        }
+        else {
+            amount = qty * price;
+        }
+        return amount;
+    }
+
+    public static double orderTotal(int qty, double price, String location) {
+        double amount;
+    
+        if(location == "UT") {
+            amount = qty * price * 1.06;
+        }
+        else
+        {
+            amount = qty * price;
+        }
+        return amount;
+    }
+}
