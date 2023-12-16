61b15e848afc617f627ba667c9958e50c0de3b39
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-15 20:49:40 -0600

    Domain 5.1.4 example

---
+++ Domain-5/ex514.java
@@ -0,0 +1,14 @@
+public class ex514 {
+    public static void main (String args[]) {
+        String [] cocoaBrands = new String[4];
+
+        cocoaBrands[0] = "Regular";
+        cocoaBrands[1] = "Dark Chocolate";
+        cocoaBrands[2] = null;
+        cocoaBrands[3] = "Sugar free";
+
+        System.out.println(cocoaBrands[1]);
+        // System.out.println(cocoaBrands[4]);
+        System.out.println(cocoaBrands[3]);
+    }
+}
