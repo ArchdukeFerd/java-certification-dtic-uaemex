1eca23c660c877710a98703b61d6e99e4cb57811
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-15 20:42:40 -0600

    Domain 5.1.2 example and compilation

---
+++ Domain-5/ex512.java
@@ -0,0 +1,8 @@
+public class ex512 {
+    public static void main (String args[]) {
+        String zero = args[0];
+        String one  = args[1];
+
+        System.out.println(Integer.parseInt(zero) / Integer.parseInt(one));
+    }
+}
