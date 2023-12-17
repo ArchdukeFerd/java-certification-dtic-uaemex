260096dd5756b6516e4393306acf97ece3b0864c
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-17 08:32:12 -0600

    q1 for training test

---
+++ Sim-Tests/q1.java
@@ -0,0 +1,10 @@
+public class q1 {
+    public static void main(String[] args) {
+        int amount = 30000;
+        double rate = .10;
+        int licensing = 1000;
+        double total = (amount + licensing) * rate;
+
+        System.out.println(total);
+    }
+}
